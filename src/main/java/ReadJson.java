d2df4fb9415e7ed0736dead442030fd862dc2fa5
Shuyi Wang <shuyi.wang@snapchat.com>
2016-10-28 15:33:31 -0700

    add arg parser

--- src/main/java/ReadJson.java
+++ src/main/java/ReadJson.java
@@ -1,6 +1,8 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 /**
@@ -15,6 +17,14 @@ public class ReadJson {
     }
 
     private void doMain(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            e.printStackTrace();
+        }
+
         SparkSession spark = SparkSession
                 .builder()
                 .appName("Read Json from Google Cloud Storage")
